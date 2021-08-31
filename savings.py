41f4e86f44cf2b7d0b197bcaefa0a539212566c2
Junhong Jia <767741316@qq.com>
2021-08-31 10:39:39 +0200

    Adding counter_END

--- savings.py
+++ savings.py
@@ -17,6 +17,16 @@ def savings_counter():
 
     return amount
 
+def savings_counter_END():
+    amount = 0
 
-amount = savings_counter()
-print("Savings at retirement", round(amount, 2))
+    for age in range(START_AGE, END_AGE):
+        amount += 6000
+        amount = amount * (1 + INTEREST)
+    
+    return amount
+
+amount1 = savings_counter()
+amount2 = savings_counter_END()
+print("Savings at retirement", round(amount1, 2))
+print("Savings at END_AGE", round(amount2, 2))
