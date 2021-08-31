5efc0562a47ce651e0f7f5a700bc2dfebf7dbefd
Zixin Rong <Zixin.Rong@outlook.com>
2021-08-31 20:38:43 +0200

    new_saving

--- savings.py
+++ savings.py
@@ -1,16 +1,16 @@
-INTEREST = 0.10
+INTEREST = 0.15
 
-START_AGE = 19
-END_AGE = 27
-RETIREMENT_AGE = 67
+START_AGE = 20
+END_AGE = 30
+RETIREMENT_AGE = 60
 
 
 def savings_counter():
     amount = 0
 
     for age in range(START_AGE, END_AGE):
-        amount += 6000
         amount = amount * (1 + INTEREST)
+        amount += 6000
 
     for age in range(END_AGE, RETIREMENT_AGE):
         amount = amount * (1 + INTEREST)
