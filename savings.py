e2f26feefb4eb4f39beb9f1c89da7c0c62ce533b
lzmeng <lzmeng@kth.se>
2021-08-31 10:45:31 +0200

    age varible

--- savings.py
+++ savings.py
@@ -1,16 +1,16 @@
-INTEREST = 0.10
+interest = 0.10
 
-START_AGE = 19
-END_AGE = 27
-RETIREMENT_AGE = 67
+start = 19
+end = 27
+retirement = 67
 
 
-def savings_counter():
+def savings_counter(START_AGE,END_AGE,RETIREMENT_AGE,INTEREST):
     amount = 0
 
     for age in range(START_AGE, END_AGE):
-        amount += 6000
         amount = amount * (1 + INTEREST)
+        amount += 6000
 
     for age in range(END_AGE, RETIREMENT_AGE):
         amount = amount * (1 + INTEREST)
@@ -18,5 +18,5 @@ def savings_counter():
     return amount
 
 
-amount = savings_counter()
+amount = savings_counter(start,end,retirement,interest)
 print("Savings at retirement", round(amount, 2))
