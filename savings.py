e501eb92a66bd771e8a1d60e82cec9e8b788721f
prachsk <prach.techa@gmail.com>
2021-08-31 10:33:18 +0200

    Adding comments

--- savings.py
+++ savings.py
@@ -1,10 +1,12 @@
+#Declaration of the interest value
 INTEREST = 0.10
 
+#Declaration of age points
 START_AGE = 19
 END_AGE = 27
 RETIREMENT_AGE = 67
 
-
+#Creating a function to calculate the saving amount with interest
 def savings_counter():
     amount = 0
 
@@ -17,6 +19,8 @@ def savings_counter():
 
     return amount
 
-
+#Call the function saving_counter and assign it to 'amount'
 amount = savings_counter()
+
+#Print the result
 print("Savings at retirement", round(amount, 2))
